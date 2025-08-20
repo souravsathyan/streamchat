e2aa944fffeae7e4fe26af52e6de794c8d32b83d
souravsathyan <souravksathyan@gmail.com>
2025-08-20 15:53:39 +0530

    completed the createAgent file

--- backend/src/serverClient.ts
+++ backend/src/serverClient.ts
@@ -1,8 +1,11 @@
-import {StreamChat} from 'stream-chat'
+import { StreamChat } from "stream-chat";
 
-const apiKey = process.env.STREAM_API_KEY as string;
-const apiSecret = process.env.STREAM_API_SECRET as string;
+export const apiKey = process.env.STREAM_API_KEY as string;
+export const apiSecret = process.env.STREAM_API_SECRET as string;
 
-if(!apiKey || !apiSecret) throw new Error ('Missing env for Stream: please check STREAM_API_KEY and STREAM_API_SECRET');
+if (!apiKey || !apiSecret)
+  throw new Error(
+    "Missing env for Stream: please check STREAM_API_KEY and STREAM_API_SECRET"
+  );
 
-export const serverClient = new StreamChat(apiKey,apiSecret)
+export const serverClient = new StreamChat(apiKey, apiSecret);
